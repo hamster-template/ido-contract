6c2d084345204f367fa4fda63c4c634b305db366
sunjianguo <1275596256@qq.com>
2023-12-15 16:35:06 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# IDO template
+This template has the basic functions of IDO projects, including purchasing, releasing, and configuring related parameters.
---
+++ contracts/IDO.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract IDO is Ownable {
+
+    using SafeERC20 for IERC20;
+
+    using SafeMath for uint256;
+
+
+    // Tokens for sale
+    IERC20 public token;
+
+    uint256 public tokenEthRate;
+
+    // Max Allocation for one wallet
+    uint256 public maxAllocation;
+
+    // Min Allocation for one wallet
+    uint256 public minAllocation;
+
+    //token release time
+    uint256 public releaseTime;
+
+    // IDO end time
+    uint256 public endTime;
+
+    // Mapping which hold how much bought every wallet
+    mapping(address => uint256) public allocations;
+
+    //user buy event
+    event PurchaseCompleted(address indexed buyer, uint256 amount);
+    // token release event
+    event TokensReleased(uint256 amount);
+
+    // contructor
+    constructor(address token_, uint256 tokenEthRate_, uint256 maxAllocation_, uint256 minAllocation_,uint256 releaseTime_,uint256 endTime_) {
+        token = IERC20(token_);
+        maxAllocation = maxAllocation_;
+        minAllocation = minAllocation_;
+        releaseTime = releaseTime_;
+        endTime = endTime_;
+        tokenEthRate = tokenEthRate_;
+    }
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= releaseTime && block.timestamp <= endTime, "IDO is not open");
+        _;
+    }
+
+    modifier onlyWhileNotReleased() {
+        require(block.timestamp < releaseTime, "Tokens have already been released");
+        _;
+    }
+
+
+    //only admin set min allocation
+    function setMinPurchaseAmount(uint256 _minAllocation) external onlyOwner {
+        minAllocation = _minAllocation;
+    }
+
+    // only admin set max allocation
+    function setMaxPurchaseAmount(uint256 _maxAllocation) external onlyOwner {
+        maxAllocation = _maxAllocation;
+    }
+
+    // only admin set token eth rate
+    function setTokenEthRate(uint256 _tokenEthRate) external onlyOwner {
+        tokenEthRate = _tokenEthRate;
+    }
+
+    // only admin set release time
+    function setReleaseTime(uint256 _releaseTime) external onlyOwner {
+        releaseTime = _releaseTime;
+    }
+
+    // only admin set end time
+    function setEndTime(uint256 _endTime) external onlyOwner {
+        endTime = _endTime;
+    }
+
+    // only admin set token
+    function setToken(address _token) external onlyOwner {
+        token = IERC20(_token);
+    }
+
+    //user buy token
+    function purchase() external payable onlyWhileOpen {
+        require(msg.value >= minAllocation, "Below minimum purchase amount");
+        require(msg.value <= maxAllocation, "Exceeds maximum purchase amount");
+        address sender = msg.sender;
+        uint256 tokenAmount = msg.value.mul(tokenEthRate);
+        require(token.balanceOf(address(this)) >= tokenAmount, "Insufficient tokens in the contract");
+        require(
+            (allocations[sender] + tokenAmount) <= maxAllocation,
+            "TokenSale: you try buy more than max allocation"
+        );
+        allocations[sender] = allocations[sender].add(msg.value);
+        token.transfer(sender, tokenAmount);
+        emit PurchaseCompleted(msg.sender, msg.value);
+    }
+
+    function releaseTokens() external onlyOwner onlyWhileNotReleased {
+        require(block.timestamp >= releaseTime, "Release time not reached yet");
+
+        uint256 totalTokens = token.balanceOf(address(this));
+        token.transfer(owner(), totalTokens);
+        emit TokensReleased(totalTokens);
+    }
+
+    function withdrawEth() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function withdrawTokens() external onlyOwner {
+        token.safeTransfer(_msgSender(), token.balanceOf(address(this)));
+    }
+
+}
